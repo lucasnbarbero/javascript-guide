f5f05ef008221c1bdf68bc3943dba1f1c5c424ce
Lucas Barbero <lucasnbarbero95@gmail.com>
2025-01-17 23:04:22 -0300

    feat: Add initial JavaScript guide and update configuration for Starlight integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      logo: {
+        light: "./src/assets/logo_javascript.webp",
+        dark: "./src/assets/logo_javascript.webp",
+      },
+      title: "Guía JavaScript",
+      customCss: ["./src/styles/custom.css"],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Introducción",
+          slug: "guides/introduction",
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
